0db168b63f4fbbc3b877e042ccb9dfe2d0a9b78f
adamyaN <adamsneupane@gmail.com>
2025-05-05 12:59:38 +0545

    class1 complete

--- lib/view/first_view.dart
+++ lib/view/first_view.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
 
-class FirstView extends StatelessWidget {
+class FirstView extends StatefulWidget {
   const FirstView({super.key});
 
   @override
+  State<FirstView> createState() => _FirstViewState();
+}
+
+class _FirstViewState extends State<FirstView> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("This is my app bar"), centerTitle: true),
-      body: Text(
-        "This is my first flutter app. \nThis is my second element. \n This is my third line.",
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text("This is my app bar"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 8),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "Enter first number",
+            ),
+            autocorrect: false,
+            autofocus: true,
+          ),
+          SizedBox(height: 8),
+          SizedBox(height: 8),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "Enter Second number",
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(onPressed: () {}, child: Text("Add")),
+          ),
+          SizedBox(height: 8),
+          Text("Result = 0"),
+        ],
       ),
     );
   }
