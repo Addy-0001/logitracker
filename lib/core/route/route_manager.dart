aa3fd0330903f9cf3029fc7320a5df2090bba6f9
adamyaN <adamsneupane@gmail.com>
2025-08-01 23:19:28 +0545

    updated route managers and added blocs

---
+++ lib/core/route/route_manager.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:logitracker/core/route/routes.dart';
+import 'package:logitracker/features/auth/presentation/view/login_view.dart';
+import 'package:logitracker/features/auth/presentation/view/register_view.dart';
+import 'package:logitracker/features/job/presentation/view/home_view.dart';
+import 'package:logitracker/features/job/presentation/view/profile_view.dart';
+import 'package:logitracker/features/job/presentation/view/splash_view.dart';
+
+Route<dynamic> onGeneratedRoute(RouteSettings settings) {
+  switch (settings.name) {
+    // Auth Pages
+    case Routes.loginPage:
+      return buildRoute(LoginView());
+    case Routes.signupPage:
+      return buildRoute(RegisterView());
+    // User Management Pages
+    case Routes.profilePage:
+      return buildRoute(ProfileView());
+    case Routes.homePage:
+      return buildRoute(HomeView());
+    case Routes.jobDetailPage:
+      // TODO: Make a job detail page that accepts job id as a parameter and sends request to that page.For now, homeView it is.
+      return buildRoute(HomeView());
+    case Routes.jobMapPage:
+      // TODO: Make a map view that accepts job id as a parameter and renders a map using any lib. For now, homeview it is.
+      return buildRoute(HomeView());
+    default:
+      return buildRoute(SplashView());
+  }
+}
+
+buildRoute(Widget child) {
+  return CupertinoPageRoute(builder: (_) => child);
+}
